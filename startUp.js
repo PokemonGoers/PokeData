6190af21d7f70e61670ef2228fc717068ea70c40
Samit Vaidya <samitsv@gmail.com>
2016-08-17 00:18:50 +0200

    code optimization using config.json and added json list of pokemons with id and name to load in collection

--- startUp.js
+++ startUp.js
@@ -1,11 +1,49 @@
 (function () {
     "use strict";
-    var express = require('express');
-    var twitter = require('./controller/twitter');
+    let express = require('express');
+    let mongoose = require ('mongoose');
+    let config = require('./config/config');
+    let mongodbConnection = "mongodb://";
 
+    let twitterStreaming = require('./controller/twitter');
+    let loadBasicPokemonDetails = require('./controller/loadBasicPokemonDetails');
+
+    //get database String
+    function getDbString(database) { 
+        return mongodbConnection + database.uri + ":" + database.port + "/" + database.collection;
+    }
+
+    function setupDataBase() {
+        console.log("Connecting to " + getDbString(config.database));
+        mongoose.connect(getDbString(config.database));
+        let db = mongoose.connection;
+
+        // MongoDB events
+        db.on('connected', function () { 
+            console.log('Mongoose connected');
+        });
+        db.on('error', function (err) {
+            console.log('Mongoose default connection error: ' + err);
+        });
+        db.on('disconnected', function () {
+            console.log('Mongoose default connection disconnected');
+        });
+        db.on('open', function () {
+            console.log("Mongoose connection open");
+        });
+    }
+    
     module.exports = {
         start: function (done) {
             console.log('App started');
+
+            setupDataBase();   
+
+            //load basic pokemon details
+            loadBasicPokemonDetails.start();
+
+            //twitter streaming
+            //twitterStreaming.startStreaming();       
         },
         stop: function (done) {
             console.log('App stopped');
