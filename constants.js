3b8ec9b1220c34d9133370b6c70e3433bc675fdd
swathi-ssunder <swathi.ssunder@gmail.com>
2016-08-17 19:57:46 +0200

    store data to local or shared db based on config

--- constants.js
+++ constants.js
@@ -3,3 +3,7 @@ global.appRoot = require('path').resolve(__dirname);
 global.__base = appRoot + '/';
 global.__appbase = appRoot + '/app/';
 global.__tmpbase = appRoot + '/tmp/';
+
+global.appConfig = {
+    IS_LOCAL_DB: false
+};
