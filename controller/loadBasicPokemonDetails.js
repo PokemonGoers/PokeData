0f529412d40163147eb68add25c18e1f24be871a
Fabian Buske <ga95jeb@mytum.de>
2016-08-18 21:05:39 +0200

    changed the pokemonlist

--- controller/loadBasicPokemonDetails.js
+++ controller/loadBasicPokemonDetails.js
@@ -8,13 +8,20 @@ function loadBasicPokemonDetails() {
     console.log('Loading Basic Pokemon Details to MongoDB');
 
     let data = JSON.stringify(pokemonListPath, function(key,value) {
-        var pokemonID, pokemonName;
+        var pokemonID, pokemonName, male, female, gender;
         if(key != 'name' && typeof value.name !== "undefined") {
             pokemonID = key;
             pokemonName = value.name;
+            male = value.gender.male;
+            female = value.gender.female;
+            if (female) {
+                gender = female;
+            } else {
+                gender = male;
+            }
             
             //add pokemonID and pokemonName to collection
-            basicPokemonDetail.create({pokemonID: pokemonID, name: pokemonName}, function(err, post){
+            basicPokemonDetail.create({pokemonID: pokemonID, name: pokemonName, gender: gender}, function(err, post){
                 if(err) {
                     if (err.name === 'MongoError' && err.code === 11000) {
                         // TODO Handling for duplicate pokemons
