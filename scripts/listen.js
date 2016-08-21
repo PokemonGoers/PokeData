f45a0de19aca6c7d8f43f07e97cc1f04341f75c5
Vivek Sethia <vivek.sethia@tum.de>
2016-08-21 20:26:00 +0200

    Modified the listener so as to take command line inputs to cater listener for different pokemon data sources.

--- scripts/listen.js
+++ scripts/listen.js
@@ -1,7 +1,23 @@
+"use strict";
+
 require(__dirname + '/../' + 'constants');
 
-// listener for rare pokemons
-var rarePokemonListener = require(__base + 'app/controllers/filler/rarePokemon');
-rarePokemonListener.fill(function () {
+const requested = process.env.npm_config_collection;
+console.log(requested);
+/*
+ * choices of collection to be filled
+ */
+var possibleListeners = [
+    'rarePokemon'
+];
+
+// when the choices of collection to be filled doesn't match, then exit the process
+if (possibleListeners.indexOf(requested) < 0) {
+    process.exit();
+}
+
+// listener for pokemons
+const listener = require(__base + 'app/controllers/filler/' + requested);
+listener.fill(function () {
     process.exit();
 });
