56267fb48dab898f3296605a210839fd7c3b95ae
Vivek Sethia <vivek.sethia@tum.de>
2016-08-23 18:02:18 +0200

    As suggested moved the collection environment variable to constants and gave it  default value.

--- app/services/common.js
+++ app/services/common.js
@@ -9,7 +9,7 @@ module.exports = {
 
             if (!error && response.statusCode === 200) {
                 const data = JSON.parse(body),
-                      srcName = process.env.npm_config_collection,
+                      srcName = collection,
                       fileName = (__tmpbase+srcName+"/"+srcName+"_"+parseInt(Math.floor(Date.now() / 1000))+".json").toString();
 
                 fs.appendFile(fileName, JSON.stringify(data.results), function(err) {
--- scripts/listen.js
+++ scripts/listen.js
@@ -2,8 +2,7 @@
 
 require(__dirname + '/../' + 'constants');
 
-const requested = process.env.npm_config_collection;
-logger.info(requested);
+logger.info(collection);
 /*
  * choices of collection to be filled
  */
@@ -12,12 +11,12 @@ var possibleListeners = [
 ];
 
 // when the choices of collection to be filled doesn't match, then exit the process
-if (possibleListeners.indexOf(requested) < 0) {
+if (possibleListeners.indexOf(collection) < 0) {
     process.exit();
 }
 
 // listener for pokemons
-const listener = require(__base + 'app/controllers/filler/' + requested);
+const listener = require(__base + 'app/controllers/filler/' + collection);
 listener.fill(function () {
     process.exit();
 });
