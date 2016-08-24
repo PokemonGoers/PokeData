90a1e99fba64af7bb60d6d0d47738ad31f6ab47b
Samit Vaidya <samitsv@gmail.com>
2016-08-24 16:52:55 +0200

    corrected the schema declaration

--- app/models/pokemonSighting.js
+++ app/models/pokemonSighting.js
@@ -1,6 +1,8 @@
-'use strict';
+"use strict";
 
 let mongoose = require('mongoose');
+var Schema   = mongoose.Schema;
+
 let pokemonSighting = new Schema({
     source        : {type: Number},
     location      : {type: String, coordinates: [Number]},
