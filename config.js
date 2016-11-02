27ecef4206ec2065db942064dba275b9e5c8ced9
jonas-he <jonius@gmx.de>
2016-11-02 23:54:28 +0100

    Update config.js

--- config.js
+++ config.js
@@ -48,6 +48,7 @@ module.exports = {
     },
     "pokemonSpawnTime" : 900000 ,// 15 minutes in milliseconds,
     "limit" : 1000,
+    "token" : "hastobefilledwithsupersecrettoken", //please choose secret token and communicate it privately
     "server": {
         "port": "8080"
     },
